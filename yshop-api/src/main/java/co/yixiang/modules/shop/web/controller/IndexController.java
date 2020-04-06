1f0cbf2808850d927f1aca57d62a4421dd9dfd51
wujingman126 <wujingman126@126.com>
2020-04-06 21:52:58 +0800

    yuelinPro

---
+++ yshop-api/src/main/java/co/yixiang/modules/shop/web/controller/IndexController.java
@@ -0,0 +1,127 @@
+/**
+ * Copyright (C) 2018-2019
+ * All rights reserved, Designed By www.yixiang.co
+ * 注意：
+ * 本软件为www.yixiang.co开发研制，未经购买不得使用
+ * 购买后可获得全部源代码（禁止转卖、分享、上传到码云、github等开源平台）
+ * 一经发现盗用、分享等行为，将追究法律责任，后果自负
+ */
+package co.yixiang.modules.shop.web.controller;
+
+import cn.hutool.core.io.file.FileReader;
+import cn.hutool.core.io.resource.ClassPathResource;
+import co.yixiang.annotation.AnonymousAccess;
+import co.yixiang.common.api.ApiResult;
+import co.yixiang.constant.ShopConstants;
+import co.yixiang.modules.shop.service.YxStoreProductService;
+import co.yixiang.modules.shop.service.YxSystemGroupDataService;
+import co.yixiang.utils.FileUtil;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @ClassName IndexController
+ * @Author hupeng <610796224@qq.com>
+ * @Date 2019/10/19
+ **/
+
+@RestController
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+@Api(value = "首页模块", tags = "商城:首页模块", description = "首页模块")
+public class IndexController {
+
+    private final YxSystemGroupDataService systemGroupDataService;
+    private final YxStoreProductService storeProductService;
+
+
+
+    @AnonymousAccess
+    @Cacheable(cacheNames = ShopConstants.YSHOP_REDIS_INDEX_KEY)
+    @GetMapping("/index")
+    @ApiOperation(value = "首页数据",notes = "首页数据")
+    public ApiResult<Map<String,Object>> index(){
+
+        Map<String,Object> map = new LinkedHashMap<>();
+        //banner
+        map.put("banner",systemGroupDataService.getDatas("routine_home_banner"));
+        //首页按钮
+        map.put("menus",systemGroupDataService.getDatas("routine_home_menus"));
+        //首页活动区域图片
+        map.put("activity",systemGroupDataService.getDatas("routine_home_activity"));
+
+
+        //精品推荐
+        map.put("bastList",storeProductService.getList(1,6,1));
+        //首发新品
+        map.put("firstList",storeProductService.getList(1,6,2));
+        //促销单品
+        map.put("benefit",storeProductService.getList(1,3,3));
+        //热门榜单
+        map.put("likeInfo",storeProductService.getList(1,3,4));
+
+        //滚动
+        map.put("roll",systemGroupDataService.getDatas("routine_home_roll_news"));
+
+        return ApiResult.ok(map);
+    }
+
+    @AnonymousAccess
+    @GetMapping("/search/keyword")
+    @ApiOperation(value = "热门搜索关键字获取",notes = "热门搜索关键字获取")
+    public ApiResult<List<String>> search(){
+        List<Map<String,Object>> list = systemGroupDataService.getDatas("routine_hot_search");
+        List<String>  stringList = new ArrayList<>();
+        for (Map<String,Object> map : list) {
+            stringList.add(map.get("title").toString());
+        }
+        return ApiResult.ok(stringList);
+    }
+
+    @AnonymousAccess
+    @PostMapping("/image_base64")
+    @ApiOperation(value = "获取图片base64",notes = "获取图片base64")
+    @Deprecated
+    public ApiResult<List<String>> imageBase64(){
+        return ApiResult.ok(null);
+    }
+
+    @AnonymousAccess
+    @GetMapping("/citys")
+    @ApiOperation(value = "获取城市json",notes = "获取城市json")
+    public ApiResult<String> cityJson(){
+        String path = "city.json";
+        String name = "city.json";
+        try {
+            File file = FileUtil.inputStreamToFile(new ClassPathResource(path).getStream(), name);
+            FileReader fileReader = new FileReader(file,"UTF-8");
+            String string = fileReader.readString();
+            System.out.println(string);
+            JSONObject jsonObject = JSON.parseObject(string);
+            return ApiResult.ok(jsonObject);
+        } catch (Exception e) {
+            e.printStackTrace();
+
+            return ApiResult.fail("无数据");
+        }
+
+    }
+
+
+
+
+
+}
---
+++ yshop-shop/src/main/java/co/yixiang/modules/shop/service/dto/YxStoreCategoryQueryCriteria.java
@@ -0,0 +1,16 @@
+package co.yixiang.modules.shop.service.dto;
+
+import co.yixiang.annotation.Query;
+import lombok.Data;
+
+/**
+* @author hupeng
+* @date 2019-10-03
+*/
+@Data
+public class YxStoreCategoryQueryCriteria{
+
+    // 模糊
+    @Query(type = Query.Type.INNER_LIKE)
+    private String cateName;
+}
